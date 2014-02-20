d901497c81044126985c6bb2dc5e563fee3c3787
Ian Norris <icstatic@gmail.com>
2014-02-20 22:20:01 +0000

    Added ASP.NET starter app.

---
+++ Source/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RationalVote.MvcApplication" Language="C#" %>
---
+++ Source/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RationalVote.Startup))]
+namespace RationalVote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
