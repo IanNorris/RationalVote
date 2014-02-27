2a2aa1496075d942bb2bbda1c88eb8f6de33c7e7
Ian Norris <icstatic@gmail.com>
2014-02-27 00:24:41 +0000

    Settings now in separate files (with templates as examples) Crypto util now has implementation User controller now more complete.

--- Source/UserController.cs
+++ Source/UserController.cs
@@ -10,129 +10,133 @@ using RationalVote.Models;
 
 namespace RationalVote
 {
-    public class UserController : Controller
-    {
-        private RationalVoteContext db = new RationalVoteContext();
+	public class UserController : Controller
+	{
+		private RationalVoteContext db = new RationalVoteContext();
 
-        // GET: /User/
-        public ActionResult Index()
-        {
-            var users = db.Users.Include(u => u.EmailVerificationTokens).Include(u => u.Profiles);
-            return View(users.ToList());
-        }
+		// GET: /User/
+		public ActionResult Index()
+		{
+			var users = db.Users.Include(u => u.EmailVerificationTokens).Include(u => u.Profiles);
+			return View(users.ToList());
+		}
 
-        // GET: /User/Details/5
-        public ActionResult Details(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
+		// GET: /User/Details/5
+		public ActionResult Details(long? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			User user = db.Users.Find(id);
+			if (user == null)
+			{
+				return HttpNotFound();
+			}
+			return View(user);
+		}
 
-        // GET: /User/Create
-        public ActionResult Create()
-        {
-            ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id");
-            ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName");
-            return View();
-        }
+		// GET: /User/Create
+		public ActionResult Create()
+		{
+			ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id");
+			ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName");
+			return View();
+		}
 
-        // POST: /User/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="Email,Password")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                user.PasswordSalt = Utility.Crypto.GenerateSalt();
+		// POST: /User/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include="Email,Password")] User user)
+		{
+			if (ModelState.IsValid)
+			{
+				string salt;
+				string hash;
+				Utility.Crypto.CreatePasswordHash( user.Email, user.Password, out salt, out hash );
+				user.PasswordSalt = salt;
+				user.PasswordHash = hash;
 
-                db.Users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+				db.Users.Add(user);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 
-            ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
-            ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
-            return View(user);
-        }
+			ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
+			ViewBag.DisplayName = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
+			return View(user);
+		}
 
-        // GET: /User/Edit/5
-        public ActionResult Edit(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
-            ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
-            return View(user);
-        }
+		// GET: /User/Edit/5
+		public ActionResult Edit(long? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			User user = db.Users.Find(id);
+			if (user == null)
+			{
+				return HttpNotFound();
+			}
+			ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
+			ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
+			return View(user);
+		}
 
-        // POST: /User/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,Email,PasswordSalt,PasswordHash,AuthenticationMethod,Verified")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
-            ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
-            return View(user);
-        }
+		// POST: /User/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include="Id,Email,PasswordSalt,PasswordHash,AuthenticationMethod,Verified")] User user)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(user).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			ViewBag.Id = new SelectList(db.EmailVerificationTokens, "Id", "Id", user.Id);
+			ViewBag.Id = new SelectList(db.Profiles, "Id", "DisplayName", user.Id);
+			return View(user);
+		}
 
-        // GET: /User/Delete/5
-        public ActionResult Delete(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
+		// GET: /User/Delete/5
+		public ActionResult Delete(long? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			User user = db.Users.Find(id);
+			if (user == null)
+			{
+				return HttpNotFound();
+			}
+			return View(user);
+		}
 
-        // POST: /User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(long id)
-        {
-            User user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// POST: /User/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(long id)
+		{
+			User user = db.Users.Find(id);
+			db.Users.Remove(user);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
--- Source/Utility/Crypto.cs
+++ Source/Utility/Crypto.cs
@@ -1,18 +1,39 @@
-﻿using System.Security.Cryptography;
+﻿using System.Configuration;
+using System.Security.Cryptography;
 
 namespace Utility
 {
 	class Crypto
 	{
+		const int IterationCount = 5000;
+		const int PasswordSaltSize = 64;
+		const int PasswordHashSize = 64;
+
 		private static RNGCryptoServiceProvider randomGenerator = new RNGCryptoServiceProvider();
+		private static string passwordSalt = ConfigurationManager.AppSettings.Get("passwordSalt");
 
 		public static string GenerateSalt()
 		{
-			byte[] bytes = new byte[64];
+			byte[] bytes = new byte[PasswordSaltSize];
 			randomGenerator.GetBytes( bytes );
 
 			string hex = System.BitConverter.ToString( bytes );
 			return hex.Replace( "-", "" );
+		}
+
+		public static void CreatePasswordHash( string email, string password, out string userSpecificSalt, out string passwordHash )
+		{
+			byte[] salt = new byte[ PasswordSaltSize ];
+			randomGenerator.GetBytes( salt );
+
+			//Store the hash we generated
+			userSpecificSalt = System.BitConverter.ToString( salt ).Replace( "-", "" );
+
+			string passwordCountents = passwordSalt + email.ToLower() + password;
+			Rfc2898DeriveBytes PBKDF2 = new Rfc2898DeriveBytes( passwordCountents, salt, IterationCount);
+
+			string hex = System.BitConverter.ToString( PBKDF2.GetBytes(PasswordHashSize) );
+			passwordHash = hex.Replace( "-", "" );
 		}
 	}
 }
