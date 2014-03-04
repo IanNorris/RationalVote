bfcf5ccde3d441d9cacd41017d6d2ba6446a86c5
Ian Norris <icstatic@gmail.com>
2014-03-04 00:23:22 +0000

    Missing files

---
+++ Source/Controllers/MailController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ActionMailer.Net.Mvc;
+using RationalVote.Models;
+using System.Configuration;
+
+namespace RationalVote.Controllers
+{
+	public class MailController : MailerBase
+	{
+		public EmailResult VerificationEmail( User model, EmailVerificationToken token )
+		{
+			To.Add( model.Email );
+			From = ConfigurationManager.AppSettings.Get("emailFrom");
+			Subject = "Please verify your account";
+			ViewBag.Token = token.Token;
+
+			return Email( "VerificationEmail" );
+		}
+	}
+}
---
+++ Source/Utility/SendMail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace Utility
+{
+	public class SendMail
+	{
+		/// <summary>
+		/// Sends an mail message
+		/// </summary>
+		/// <param name="from">Sender address</param>
+		/// <param name="to">Recepient address</param>
+		/// <param name="bcc">Bcc recipient</param>
+		/// <param name="cc">Cc recipient</param>
+		/// <param name="subject">Subject of mail message</param>
+		/// <param name="body">Body of mail message</param>
+		/// <param name="html">Is the body HTML?</param>
+		public static void SendMailMessage( string from, string to, string bcc, string cc, string subject, string body, bool html )
+		{
+			MailMessage message = new MailMessage();
+
+			message.From = new MailAddress( from );
+			message.To.Add( new MailAddress( to ) );
+			message.Subject = subject;
+			message.Body = body;
+			message.IsBodyHtml = html;
+			message.Priority = MailPriority.Normal;
+
+			if( ( bcc != null ) && ( bcc != string.Empty ) )
+			{
+				message.Bcc.Add( new MailAddress( bcc ) );
+			}
+
+			if( ( cc != null ) && ( cc != string.Empty ) )
+			{
+				message.CC.Add( new MailAddress( cc ) );
+			}
+
+			SmtpClient smtp = new SmtpClient();
+			smtp.Send( message );
+		}
+	}
+}
