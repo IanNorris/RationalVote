8cc7fa6a16b8163decec7bc9d41ad162b4087da9
Ian Norris <icstatic@gmail.com>
2014-03-20 22:39:09 +0000

    Moved third party libs into ThirdParty folder Changed 'Log off' to 'Sign out'.

--- Source/Configs/BundleConfig.cs
+++ Source/Configs/BundleConfig.cs
@@ -9,34 +9,34 @@ namespace RationalVote
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/Scripts/ThirdParty/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate.js",
-						"~/Scripts/jquery.validate.unobtrusive.js",
-						"~/Scripts/jquery.validate.unobtrusive-custom.js"));
+						"~/Scripts/ThirdParty/jquery.validate.js",
+						"~/Scripts/ThirdParty/jquery.validate.unobtrusive.js",
+						"~/Scripts/ThirdParty/jquery.validate.unobtrusive-custom.js"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+						"~/Scripts/ThirdParty/modernizr-*"));
 
 			bundles.Add( new ScriptBundle( "~/bundles/zxcvbn" ).Include(
-						"~/Scripts/zxcvbn.js",
+						"~/Scripts/ThirdParty/zxcvbn.js",
 						"~/Scripts/zxcvbn-register.js") );
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
+					  "~/Scripts/ThirdParty/bootstrap.js",
+					  "~/Scripts/ThirdParty/respond.js"));
 
 			bundles.Add( new ScriptBundle( "~/bundles/errorscripts" ).Include(
-					  "~/Scripts/jquery.backstretch.js",
+					  "~/Scripts/ThirdParty/jquery.backstretch.js",
 					  "~/Scripts/errorscripts.js" ) );
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
+					  "~/Content/ThirdParty/bootstrap.css",
 					  "~/Content/Theme/default.css",
-					  "~/Content/font-awesome/css/font-awesome.css"));
+					  "~/Content/ThirdParty/font-awesome/css/font-awesome.css"));
 
 			bundles.Add( new StyleBundle( "~/Content/csserror" ).Include(
 					  "~/Content/Theme/error.css" ) );
