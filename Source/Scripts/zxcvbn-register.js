9046b9bd263cb4de711d5ef7aa5ca133bd4d1d45
Ian Norris <icstatic@gmail.com>
2014-03-28 01:33:48 +0000

    Fixes to zxcvbn script

--- Source/Scripts/zxcvbn-register.js
+++ Source/Scripts/zxcvbn-register.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
-    $('#Password').keyup(function(event){
-        var password = $('#Password').val();
-        var result = zxcvbn(password);
-        score100 = (result.score * 25);
+$(document).ready(function()
+{
+	var checker = function()
+	{
+		var password = $('#RegisterPassword').val();
+		var result = zxcvbn(password);
+		score100 = (result.score * 25);
 
-        // progress bar based on score
-        $('#password_strength_bar').css('width', score100+'%');
-        $('#password_strength_bar').removeClass("progress-bar-danger progress-bar-warning progress-bar-success").addClass(result.score < 3 ? "progress-bar-danger" : result.score === 3 ? "progress-bar-warning" : "progress-bar-success");
+		if( password.length > 0 )
+		{
+			score100 = Math.max( score100, 5 );
+		}
 
-    });
+		// progress bar based on score
+		$('#password_strength_bar').css('width', score100+'%');
+		$('#password_strength_bar').removeClass("progress-bar-danger progress-bar-warning progress-bar-success").addClass(result.score < 3 ? "progress-bar-danger" : result.score === 3 ? "progress-bar-warning" : "progress-bar-success");
+
+	};
+
+	$('#RegisterPassword')
+		.unbind(".charCounter")
+		.bind("keydown", checker )
+		.bind("keypress", checker )
+		.bind("keyup", checker )
+		.bind("focus", checker )
+		.bind("mouseover", checker )
+		.bind("mouseout", checker )
+		.bind("paste", checker )
+		.bind("click", checker );
 });
