3388d3ffee30549e09eaa0340fe26b56152def0e
Ian Norris <icstatic@gmail.com>
2014-04-12 00:40:20 +0100

    Fixed display email name

--- Source/Controllers/MailController.cs
+++ Source/Controllers/MailController.cs
@@ -10,10 +10,18 @@ namespace RationalVote.Controllers
 {
 	public class MailController : MailerBase
 	{
+		public static string GetFromEmail( string purpose )
+		{
+			return String.Format( "\"{0} {1}\" <{2}>", 
+				ConfigurationManager.AppSettings.Get("siteTitle"),
+				purpose,
+				ConfigurationManager.AppSettings.Get("emailFrom") );
+		}
+
 		public EmailResult VerificationEmail( User model, EmailVerificationToken token )
 		{
 			To.Add( model.Email );
-			From = ConfigurationManager.AppSettings.Get("emailFrom");
+			From = GetFromEmail( "Verification" );
 			Subject = "Please verify your account";
 			ViewBag.Token = token.Token;
 
@@ -23,7 +31,7 @@ namespace RationalVote.Controllers
 		public EmailResult ExceptionEmail( HttpException e )
 		{
 			To.Add( ConfigurationManager.AppSettings.Get( "ServerAdmin" ) );
-			From = ConfigurationManager.AppSettings.Get("emailFrom");
+			From = GetFromEmail( "Exception" );
 			Subject = "Site exception";
 			ViewBag.Exception = e.ToString();
 
